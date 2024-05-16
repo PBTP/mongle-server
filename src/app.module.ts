1a20da33c03fb59edaa16bd4c970e11a28ad349d
Jihoon Ko <emibgo2@naver.com>
2024-05-16 18:04:47 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { loadParameterStoreValue } from './env/ssm-config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      load: [loadParameterStoreValue],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const datasource = JSON.parse(configService.get('datasource'));
+
+        return {
+          type: 'postgres',
+          host: datasource.host,
+          port: datasource.port,
+          username: datasource.username,
+          password: datasource.password,
+          database: datasource.database,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
