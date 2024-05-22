885af6ede4229504236ed49c99aac5cb386423c8
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2024-05-22 19:11:03 +0900

    Revert "DMVM-137 feat: 인증/인가"

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,6 @@ import { loadParameterStoreValue } from './env/ssm-config.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PreRegistrationServeyModule } from './pre-registration-servey/pre-registration-servey.module';
 import { EmailModule } from './email/email.module';
-import { CustomerModule } from './customer/customer.module';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { AuthModule } from './auth/auth.module';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
-import { CacheModule } from "./common/cache/cache.module";
 
 @Module({
   imports: [
@@ -21,7 +16,7 @@ import { CacheModule } from "./common/cache/cache.module";
     }),
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => {
-        const datasource = JSON.parse(configService.get('datasource/db'));
+        const datasource = JSON.parse(configService.get('datasource'));
 
         return {
           type: 'postgres',
@@ -32,26 +27,12 @@ import { CacheModule } from "./common/cache/cache.module";
           database: datasource.database,
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
           synchronize: false,
-          namingStrategy: new SnakeNamingStrategy(),
-        };
-      },
-      inject: [ConfigService],
-    }),
-    RedisModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => {
-        const datasource = JSON.parse(configService.get('datasource/redis'));
-        return{
-          config: datasource,
-          readyLog: true
         };
       },
       inject: [ConfigService],
     }),
     EmailModule,
     PreRegistrationServeyModule,
-    CacheModule,
-    CustomerModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
