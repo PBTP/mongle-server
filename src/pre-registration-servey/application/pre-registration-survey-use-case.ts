68703ccb8d6fe187fc0866322bc14b7f6f19d3f7
Seokmin Jung <79754613+shine-jung@users.noreply.github.com>
2024-05-22 10:08:47 +0900

    DMVM-134 feat: 이메일 서비스에 디스코드 알림 추가

--- src/pre-registration-servey/application/pre-registration-survey-use-case.ts
+++ src/pre-registration-servey/application/pre-registration-survey-use-case.ts
@@ -1,28 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PreRegistrationSurveyRequest } from './pre-registration-survey-request';
 import { EmailService } from 'src/email/email.service';
+import { EmailContentGenerator } from './email-content-generator';
 
 @Injectable()
 export class PreRegistrationSurveyUseCase {
   constructor(private readonly emailService: EmailService) {}
 
   async execute(request: PreRegistrationSurveyRequest): Promise<void> {
-    const subject = '사전 등록 설문';
-    const text = `사전 등록 설문이 완료되었습니다. ${JSON.stringify(request)}`;
-    const html = `
-      <div>
-        <h1>사전 등록 설문</h1>
-        <p>사전 등록 설문이 완료되었습니다.</p>
-        <p>이름: ${request.name}</p>
-        <p>이메일: ${request.email}</p>
-        <p>휴대폰 번호: ${request.phoneNumber}</p>
-        <p>사업장명: ${request.businessName}</p>
-        <p>지역: ${request.region}</p>
-        <p>예약 플랫폼: ${request.reservationPlatform}</p>
-        <p>SNS 연락 가능 여부: ${request.snsContact}</p>
-        <p>전화 인터뷰 가능 여부: ${request.phoneInterview}</p>
-      </div>
-    `;
+    const subject = '[몽글몽글] 사전 등록 신청이 완료되었습니다.';
+    const text = EmailContentGenerator.generateTextContent(request);
+    const html = EmailContentGenerator.generateHtmlContent(request);
 
     await this.emailService.sendEmail(request.email, subject, text, html);
   }
