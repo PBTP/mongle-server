adaa2d057707fd86ec337448fca21a9c51ed7705
Jihoon Ko <jhko@nemosys.kr>
2024-07-10 16:55:21 +0900

    feature: socket exception filter 수정 - super 호출 제거 - exception 포맷 수정

--- src/chat/application/http-to-socket-exception.filter.ts
+++ src/chat/application/http-to-socket-exception.filter.ts
@@ -6,16 +6,7 @@ export class HttpToSocketExceptionFilter extends BaseWsExceptionFilter<HttpExcep
   // BaseWsExceptionFilter를 상속하면 Ws관련 Exception을 만들 수 있다.
 
   catch(exception: HttpException, host: ArgumentsHost): void {
-    super.catch(exception, host);
-
     const socket = host.switchToWs().getClient(); // 소켓 가져오기
-    socket.emit(
-      // 현재 소켓에다가만 emit하기
-      'exception', // 이벤트 이름
-      {
-        // 실제 응답에서 받는 {메세지 형태들}
-        data: exception.getResponse(),
-      },
-    );
+    socket.emit('exception', exception.getResponse());
   }
 }
