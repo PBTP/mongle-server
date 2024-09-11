e050f3a50b8eb0f0617cb4d9d752ca8b54fcece5
Jihoon Ko <jhko@nemosys.kr>
2024-09-11 13:30:23 +0900

    feature: customer customerAddress 추가

--- src/customer/application/customer.service.ts
+++ src/customer/application/customer.service.ts
@@ -20,19 +20,18 @@ export class CustomerService implements IUserService {
     @InjectRepository(Customer)
     private readonly customerRepository: Repository<Customer>,
     private readonly securityService: SecurityService,
-    private readonly imageService: ImageService
-  ) {
-  }
+    private readonly imageService: ImageService,
+  ) {}
 
   async create(dto: CustomerDto): Promise<Customer> {
     return await this.customerRepository.save(
-      this.customerRepository.create(dto)
+      this.customerRepository.create(dto),
     );
   }
 
   async findOne(
     dto: Partial<AuthDto>,
-    encrypt: boolean = false
+    encrypt: boolean = false,
   ): Promise<Customer> {
     const query = this.customerRepository
       .createQueryBuilder('C')
@@ -41,7 +40,7 @@ export class CustomerService implements IUserService {
 
     if (dto.userId) {
       query.andWhere('C.customer_id = :customer_id', {
-        customer_id: dto.userId
+        customer_id: dto.userId,
       });
     }
 
@@ -56,12 +55,14 @@ export class CustomerService implements IUserService {
 
     if (encrypt) {
       customer.customerPhoneNumber = this.securityService.decrypt(
-        customer.customerPhoneNumber
+        customer.customerPhoneNumber,
       );
 
-      customer.customerAddress = this.securityService.decrypt(customer.customerAddress);
+      customer.customerAddress = this.securityService.decrypt(
+        customer.customerAddress,
+      );
       customer.customerDetailAddress = this.securityService.decrypt(
-        customer.customerDetailAddress
+        customer.customerDetailAddress,
       );
     }
 
@@ -71,20 +72,18 @@ export class CustomerService implements IUserService {
   async update(dto: Partial<CustomerDto>): Promise<CustomerDto> {
     if (dto.phoneNumber || dto.customerPhoneNumber) {
       dto.phoneNumber = this.securityService.encrypt(
-        dto.phoneNumber ?? dto.customerPhoneNumber
+        dto.phoneNumber ?? dto.customerPhoneNumber,
       );
     }
 
     if (dto.customerDetailAddress) {
       dto.customerDetailAddress = this.securityService.encrypt(
-        dto.customerDetailAddress
+        dto.customerDetailAddress,
       );
     }
 
     if (dto.customerAddress) {
-      dto.customerAddress = this.securityService.encrypt(
-        dto.customerAddress
-      );
+      dto.customerAddress = this.securityService.encrypt(dto.customerAddress);
     }
 
     return await this.findOne(dto)
@@ -93,7 +92,8 @@ export class CustomerService implements IUserService {
           customer.customerName = dto.customerName ?? customer.customerName;
           customer.customerPhoneNumber =
             dto.phoneNumber ?? customer.customerPhoneNumber;
-          customer.customerAddress = dto.customerAddress ?? customer.customerAddress;
+          customer.customerAddress =
+            dto.customerAddress ?? customer.customerAddress;
           customer.customerDetailAddress =
             dto.customerDetailAddress ?? customer.customerDetailAddress;
           customer.refreshToken = dto.refreshToken ?? customer.refreshToken;
@@ -105,11 +105,11 @@ export class CustomerService implements IUserService {
         if (customer && dto.presignedUrlDto) {
           const presignedUrlDto = await this.imageService.generatePreSignedUrls(
             customer.uuid,
-            [dto.presignedUrlDto]
+            [dto.presignedUrlDto],
           );
 
           const customerDto = toDto(CustomerDto, customer);
-          customerDto.presignedUrlDto = presignedUrlDto.find(() => true);
+          customerDto.presignedUrlDto = presignedUrlDto.find(() => true); // The first truthy vo (!undefined, !null ...)
           return customerDto;
         }
         return customer;
@@ -123,7 +123,7 @@ export class CustomerService implements IUserService {
       userId: customer.customerId,
       userType: this.userType,
       phoneNumber: customer.customerPhoneNumber,
-      authProvider: customer.authProvider
+      authProvider: customer.authProvider,
     };
   }
 }
