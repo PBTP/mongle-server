fed46c219f7a0b95f56da132813aa27300d23401
hyo <hyoseonee0729@naver.com>
2025-01-19 21:56:36 +0900

    test: PetSerivce 테스트코드 작성중

--- test/mock/pet/fake.pet.checklist.repository.ts
+++ test/mock/pet/fake.pet.checklist.repository.ts
@@ -1,34 +1,46 @@
-import {IPetChecklistRepository} from "../../../src/pet/port/pet.checklist.repository";
-import {ChecklistType, PetChecklistCategory, PetChecklistEntity} from "../../../src/schemas/pet-checklist.entity";
-import {PetChecklist} from "../../../src/pet/pet.checklist.domain";
+import { PetChecklist } from '../../../src/pet/pet.checklist.domain';
+import { IPetChecklistRepository } from '../../../src/pet/port/pet.checklist.repository';
+import {
+  ChecklistType,
+  PetChecklistCategory,
+  PetChecklistEntity,
+} from '../../../src/schemas/pet-checklist.entity';
 
 export class FakePetChecklistRepository implements IPetChecklistRepository {
-    private readonly checkLists: PetChecklistEntity[] = [];
+  private readonly checkLists: PetChecklistEntity[] = [];
 
-    async findCheckList(
-        category: PetChecklistCategory,
-        type: ChecklistType,
-        petId: number | null,
-    ): Promise<PetChecklistEntity[]>{
-        return this.checkLists.filter((checklist) => {
-            const matchesType = (!type || checklist.petChecklistType === type);
-            const matchesCategory = (!category || checklist.petChecklistCategory === category);
+  async findCheckList(
+    category: PetChecklistCategory,
+    type: ChecklistType,
+    petId: number | null,
+  ): Promise<PetChecklistEntity[]> {
+    return this.checkLists.filter((checklist) => {
+      const matchesType = !type || checklist.petChecklistType === type;
+      const matchesCategory =
+        !category || checklist.petChecklistCategory === category;
+      const matchesPetId =
+        !petId ||
+        checklist.petChecklistAnswers?.some((answer) => answer.petId === petId);
+      return matchesCategory && matchesType && matchesPetId;
+    });
+  }
 
-            return matchesCategory && matchesType;
-        });
-    };
+  async findByIds(ids: number[]): Promise<PetChecklistEntity[]> {
+    const findCheckList: PetChecklistEntity[] = this.checkLists.filter(
+      (cl: PetChecklistEntity) => {
+        return ids.includes(cl.petChecklistId);
+      },
+    );
+    if (findCheckList.length === 0) {
+      throw new Error('PetChecklistEntity 목록을 찾을 수 없습니다.');
+    }
+    return findCheckList;
+  }
 
-    async findByIds(ids: number[]): Promise<PetChecklistEntity[]>{
-        const findCheckList: PetChecklistEntity[] = this.checkLists.filter((cl:PetChecklistEntity)=>{
-            return ids.includes(cl.petChecklistId);
-        })
-        if(findCheckList.length===0){
-            throw new Error('PetChecklistEntity 목록을 찾을 수 없습니다.');
-        }
-        return findCheckList;
-    };
-
-    async create(dto: PetChecklist): Promise<PetChecklistEntity>{
-        return new PetChecklistEntity();
-    };
+  async create(checklist: PetChecklist): Promise<PetChecklistEntity> {
+    const entity = PetChecklistEntity.create(checklist);
+    entity.petChecklistId = this.checkLists.length + 1;
+    this.checkLists.push(entity);
+    return entity;
+  }
 }
--- test/unit/pet/application/pet.service.test.ts
+++ test/unit/pet/application/pet.service.test.ts
@@ -2,7 +2,15 @@ import { Builder } from 'builder-pattern';
 import { AuthProvider } from '../../../../src/auth/presentation/user.dto';
 import { Customer } from '../../../../src/customer/customer.domain';
 import { PetService } from '../../../../src/pet/application/pet.service';
-import { PetDto } from '../../../../src/pet/presentation/pet.dto';
+import {
+  PetChecklistAnswerDto,
+  PetDto,
+} from '../../../../src/pet/presentation/pet.dto';
+import {
+  ChecklistType,
+  PetChecklistCategory,
+} from '../../../../src/schemas/pet-checklist.entity';
+import { Gender } from '../../../../src/schemas/pets.entity';
 import { FakeDateHolder, FakeUuidHolder } from '../../../mock/fake.holder';
 import { FakeBreedRepository } from '../../../mock/pet/fake.breed.repository';
 import { FakePetChecklistAnswerRepository } from '../../../mock/pet/fake.pet.checklist-answer.repository';
@@ -13,6 +21,7 @@ import { FakePetRepository } from '../../../mock/pet/fake.pet.repository';
 describe('PetService', () => {
   let service: PetService;
   let fakeBreedRepository: FakeBreedRepository;
+  let fakePetChecklistRepository: FakePetChecklistRepository;
 
   const customer: Customer = {
     customerId: 1,
@@ -31,6 +40,20 @@ describe('PetService', () => {
       },
       new FakeUuidHolder(),
     );
+    fakePetChecklistRepository = new FakePetChecklistRepository();
+    fakePetChecklistRepository.create({
+      petChecklistId: 1,
+      petChecklistType: ChecklistType.ANSWER,
+      petChecklistCategory: PetChecklistCategory.HEALTH,
+      petChecklistContent: '건강하다',
+    });
+
+    const choiceRepository = new FakePetChecklistChoiceAnswerRepository();
+    choiceRepository.create({
+      petChecklistChoiceId: 1,
+      petChecklistId: 1,
+      petId: 1,
+    });
 
     const date = new Date();
     service = new PetService(
@@ -44,75 +67,82 @@ describe('PetService', () => {
     );
   });
 
-  // 👍🏼describe('create', () => {
-  //   test('반려동물 엔티티 생성', async () => {
-  //     const petDto: PetDto = Builder<PetDto>()
-  //       .petName('몽글이')
-  //       .petGender(Gender.MALE)
-  //       .petBirthdate(new Date())
-  //       .petWeight(10)
-  //       .neuteredYn(true)
-  //       .personality('cute')
-  //       .vaccinationStatus('미완료')
-  //       .appointments([])
-  //       .breedId(1)
-  //       .build();
+  describe('create', () => {
+    test('반려동물 엔티티 생성', async () => {
+      const petDto: PetDto = Builder<PetDto>()
+        .petName('몽글이')
+        .petGender(Gender.MALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .appointments([])
+        .breedId(1)
+        .build();
 
-  //     const pet = await service.create(petDto, customer);
+      const pet = await service.create(petDto, customer);
 
-  //     expect(pet).toBeDefined();
-  //     expect(pet.petName).toBe('몽글이');
-  //     expect(pet.breed.breedId).toBe(1);
-  //     // expect(pet.customer.customerId).toBe(1);
-  //     expect(pet.customer.customerName).toBe('홍길동');
-  //     expect(pet.customer.authProvider).toBe(AuthProvider.BASIC);
-  //   });
-  // });
+      expect(pet).toBeDefined();
+      expect(pet.petName).toBe('몽글이');
+      expect(pet.breed.breedId).toBe(1);
+      // expect(pet.customer.customerId).toBe(1);
+      expect(pet.customer.customerName).toBe('홍길동');
+      expect(pet.customer.authProvider).toBe(AuthProvider.BASIC);
+    });
+  });
 
-  // 👍🏼describe('findAll', () => {
-  //   test('특정 고객의 전체 반려동물 조회', async () => {
-  //     const pet1: PetDto = Builder<PetDto>()
-  //       .petName('몽글이')
-  //       .petGender(Gender.FEMALE)
-  //       .petBirthdate(new Date())
-  //       .petWeight(10)
-  //       .neuteredYn(true)
-  //       .personality('cute')
-  //       .vaccinationStatus('미완료')
-  //       .breedId(1)
-  //       .neuteredYn(true)
-  //       .appointments([])
-  //       .build();
+  describe('findAll', () => {
+    test('특정 고객의 전체 반려동물 조회', async () => {
+      const pet1: PetDto = Builder<PetDto>()
+        .petName('몽글이')
+        .petGender(Gender.FEMALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .breedId(1)
+        .neuteredYn(true)
+        .appointments([])
+        .build();
 
-  //     const pet2: PetDto = Builder<PetDto>()
-  //       .petName('동글이')
-  //       .petGender(Gender.MALE)
-  //       .petBirthdate(new Date())
-  //       .petWeight(10)
-  //       .neuteredYn(true)
-  //       .personality('cute')
-  //       .vaccinationStatus('미완료')
-  //       .breedId(1)
-  //       .neuteredYn(true)
-  //       .appointments([])
-  //       .build();
+      const pet2: PetDto = Builder<PetDto>()
+        .petName('동글이')
+        .petGender(Gender.MALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .breedId(1)
+        .neuteredYn(true)
+        .appointments([])
+        .build();
 
-  //     await service.create(pet1, customer);
-  //     await service.create(pet2, customer);
+      await service.create(pet1, customer);
+      await service.create(pet2, customer);
 
-  //     const pets = await service.findAll(customer);
+      const pets = await service.findAll(customer);
 
-  //     expect(pets).toBeDefined();
-  //     expect(pets.length).toBe(2);
-  //     expect(pets[0].petName).toBe('몽글이');
-  //     expect(pets[1].petName).toBe('동글이');
-  //   });
-  // });
+      expect(pets).toBeDefined();
+      expect(pets.length).toBe(2);
+      expect(pets[0].petName).toBe('몽글이');
+      expect(pets[1].petName).toBe('동글이');
+    });
+  });
 
   describe('findOne', () => {
     test('반려동물 단일 조회', async () => {
       const petDto: PetDto = Builder<PetDto>()
         .petName('몽글이')
+        .petGender(Gender.MALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .appointments([])
         .breedId(1)
         .build();
       const createdPet = await service.create(petDto, customer);
@@ -124,73 +154,81 @@ describe('PetService', () => {
     });
   });
 
-  // describe('update', () => {
-  //   test('반려동물 정보 수정', async () => {
-  //     const petDto: PetDto = Builder<PetDto>()
-  //       .petName('몽글이')
-  //       .petGender(Gender.MALE)
-  //       .petBirthdate(new Date())
-  //       .petWeight(10)
-  //       .neuteredYn(true)
-  //       .personality('cute')
-  //       .vaccinationStatus('미접종')
-  //       .breedId(1)
-  //       .build();
+  describe('update', () => {
+    test('반려동물 정보 수정', async () => {
+      const petDto: PetDto = Builder<PetDto>()
+        .petName('몽글이')
+        .petGender(Gender.MALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .appointments([])
+        .breedId(1)
+        .build();
 
-  //     const prevPet = await service.create(petDto, customer);
-  //     const updatedPet = await service.update(
-  //       prevPet.petId,
-  //       { petName: '동글이' },
-  //       customer,
-  //     );
+      const prevPet = await service.create(petDto, customer);
+      const updatedPet = await service.update(
+        prevPet.petId,
+        { petName: '동글이' },
+        customer,
+      );
 
-  //     expect(updatedPet).toBeDefined();
-  //     expect(updatedPet.petName).toBe('동글이');
-  //     expect(updatedPet.petId).toBe(prevPet.petId);
-  //   });
-  // });
+      expect(updatedPet).toBeDefined();
+      expect(updatedPet.petName).toBe('동글이');
+      expect(updatedPet.petId).toBe(prevPet.petId);
+    });
+  });
 
-  // 👍🏼describe('findCheckList', () => {
-  //   test('반려동물 체크리스트 조회', async () => {
-  //     const checklist = await service.findCheckList(
-  //       PetChecklistCategory.HEALTH,
-  //       ChecklistType.ANSWER,
-  //       null,
-  //     );
+  describe('findCheckList', () => {
+    test('반려동물 체크리스트 조회', async () => {
+      const checklist = await service.findCheckList(
+        PetChecklistCategory.HEALTH,
+        ChecklistType.ANSWER,
+        null,
+      );
 
-  //     expect(checklist).toBeDefined();
-  //     expect(Array.isArray(checklist)).toBe(true);
-  //   });
-  // });
+      expect(checklist).toBeDefined();
+      expect(Array.isArray(checklist)).toBe(true);
+    });
+  });
 
-  // describe('answerChecklist', () => {
-  //   test('반려동물 체크리스트 답변', async () => {
-  //     const petDto: PetDto = Builder<PetDto>()
-  //       .petName('멍멍이')
-  //       .breedId(1)
-  //       .build();
+  describe('answerChecklist', () => {
+    test('반려동물 체크리스트 답변', async () => {
+      const petDto: PetDto = Builder<PetDto>()
+        .petName('몽글이')
+        .petGender(Gender.MALE)
+        .petBirthdate(new Date())
+        .petWeight(10)
+        .neuteredYn(true)
+        .personality('cute')
+        .vaccinationStatus('미완료')
+        .appointments([])
+        .breedId(1)
+        .build();
 
-  //     const pet = await service.create(petDto, customer);
+      const pet = await service.create(petDto, customer);
 
-  //     const answers: PetChecklistAnswerDto[] = [
-  //       {
-  //         petId: pet.petId,
-  //         petChecklistId: 2,
-  //         petChecklistChoiceId: 1,
-  //         petChecklistAnswer: '귀엽다',
-  //         checked: true,
-  //       },
-  //     ];
+      const answers: PetChecklistAnswerDto[] = [
+        {
+          petId: pet.petId,
+          petChecklistId: 1,
+          petChecklistChoiceId: 1,
+          petChecklistAnswer: '귀엽다',
+          checked: true,
+        },
+      ];
 
-  //     await service.answerChecklist(pet.petId, answers, customer);
+      await service.answerChecklist(pet.petId, answers, customer);
 
-  //     const checklist = await service.findCheckList(
-  //       PetChecklistCategory.HEALTH,
-  //       ChecklistType.ANSWER,
-  //       pet.petId,
-  //     );
+      const checklist = await service.findCheckList(
+        PetChecklistCategory.HEALTH,
+        ChecklistType.ANSWER,
+        pet.petId,
+      );
 
-  //     expect(checklist[0].petChecklistAnswer).toBe('귀엽다');
-  //   });
-  // });
+      expect(checklist[0].petChecklistAnswer).toBe('귀엽다');
+    });
+  });
 });
