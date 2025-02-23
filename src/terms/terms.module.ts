3dff5ce7c620177f745a715e0f48fb652bfe20aa
emibgo2 <emibgo2@naver.com>
2025-02-23 17:55:42 +0900

    fix: merge conflicts resolved

---
+++ src/terms/terms.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TermController } from "./presentation/terms.controller";
+import { TermService } from "./application/terms.service";
+import { TERM_REPOSITORY, TermRepository } from "./port/terms.repository";
+import { CUSTOMER_TERM_REPOSITORY, CustomerTermRepository } from "./port/customer-terms.repository";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            TermEntity,
+            CustomerTermEntity,
+        ])
+    ],
+    controllers: [TermController],
+    providers: [
+        TermService,
+        {provide: TERM_REPOSITORY, useClass: TermRepository},
+        {provide: CUSTOMER_TERM_REPOSITORY, useClass: CustomerTermRepository}
+    ],
+    exports: [TermService]
+})
+export class TermModule {}
