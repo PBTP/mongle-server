85cf1667ced955746cb16f658af74cd02e6f3eed
Hyo <lovellehyo@gmail.com>
2025-02-28 16:50:54 +0900

    [feature] 약관 모듈

--- src/terms/terms.module.ts
+++ src/terms/terms.module.ts
@@ -4,6 +4,8 @@ import { TermController } from "./presentation/terms.controller";
 import { TermService } from "./application/terms.service";
 import { TERM_REPOSITORY, TermRepository } from "./port/terms.repository";
 import { CUSTOMER_TERM_REPOSITORY, CustomerTermRepository } from "./port/customer-terms.repository";
+import { TermEntity } from "../schemas/terms.entity";
+import { CustomerTermEntity } from "../schemas/customer-terms.entity";
 
 @Module({
     imports: [
